3d34330a879ffc2140d3adae43c22f902a213086
Chris Dorsman <39407105+cdorsman@users.noreply.github.com>
2025-08-12 17:22:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,5 +12,5 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    port = int(os.environ.get('PORT', 8000))
+    port = int(os.environ.get('PORT', 443))
     app.run(host='0.0.0.0', port=port)
