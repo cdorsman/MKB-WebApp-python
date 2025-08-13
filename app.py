029931e6de8eec47efe0bf96f133e48097658d3a
Chris Dorsman <39407105+cdorsman@users.noreply.github.com>
2025-08-14 01:55:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 from flask import Flask
 import os
 
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    @app.route('/')
-    def hello():
-        return '<h1>Hello, MKB-bedrijf!</h1>'
-
-    return app
+@app.route('/')
+def hello():
+    return '<h1>Hello, MKB-bedrijf!</h1>'
 
 if __name__ == '__main__':
-    app = create_app()
     port = int(os.environ.get('PORT', 8000))
     app.run(host='0.0.0.0', port=port)
